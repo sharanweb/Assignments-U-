d03468ba0603f2cd7902b6c1bf00f63a7e043f08
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-03-07 12:25:07 +0530

    Update index.js

--- eval/index.js
+++ eval/index.js
@@ -3,40 +3,45 @@ const app = express();
 
 app.use = logger;  //logger for all functions
 
-app.get("/books", (req,res)=>{
+//routehandler 1
+app.get("/books", (req,res)=>{   
     res.send({route: req.path});
 })
 
-app.get("/libraries",checkPermission("/libraries"), (req,res)=>{
+
+//routehandler 2 
+app.get("/libraries",checkPermission("librarian"), (req,res)=>{  
     console.log("libraries");
-    res.send({route: req.path});
+    res.send({route: req.path , permission: req.permission});
 })
 
+
+//routehandler 3
 app.get("/authors", checkPermission("authors"), (req,res)=>{
-    res.send({route: req.path});
+    res.send({route: req.path , permission: req.permission});
 })
 
-//middlewares for authors and libraries
+//Common middleware for authors and libraries function 
 function checkPermission(something){
     return function (req,res,next){
-        if(something==="/libraries" || something === "/authors"){
-            permission = true;
-            next();
-        }else{
-            permission = false;
-            next();
-
+        if(something=== 'authors'){
+            req.permission = true;
+            
+        }else if(something === "librarian"){
+            req.permission = true;
         }
+        next();
     }
 }
 
-
+//common logger function
 function logger(req,res,next){
     console.log("fetching before");
     next();
     console.log("function done bye");
 }
 
+//listener here
 app.listen(5001,()=>{
     console.log("Listeninig on port 5001");
 })
